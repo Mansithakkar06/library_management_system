247342ec8320e291d939cc3d94706acd2c7e64ff
Mansithakkar06 <mansithakkar203@gmail.com>
2024-09-01 14:42:21 +0530

    test for available books

--- test_library.py
+++ test_library.py
@@ -25,6 +25,17 @@ class TestLibraryManagementSystem(unittest.TestCase):
         self.library.borrow_book("1234567890")
         self.library.return_book("1234567890")
         self.assertFalse(book.is_borrowed)
+
+    #test for available books
+    def test_available_book(self):
+        book1 = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
+        book2 = Book(isbn="2345678910", title="java", author="james goslin", year=2023)
+        self.library.add_book(book1)
+        self.library.add_book(book2)
+        self.library.borrow_book("1234567890")
+        available_books=self.library.view_available_books()
+        self.assertNotIn(book1,available_books)
+        self.assertIn(book2,available_books)
         
 if __name__ == "__main__":
     unittest.main()
