c3fd25576389e30872cf81fb93047ed83e925fc5
Mansithakkar06 <mansithakkar203@gmail.com>
2024-09-01 16:56:48 +0530

    added comments

--- library.py
+++ library.py
@@ -4,38 +4,38 @@ class Book:
         self.title=title
         self.author=author
         self.year=year
-        self.is_borrowed=False
+        self.is_borrowed=False #to indicate that book is borrowed or not by default it is false it will be true when book is borrowed
 
     def __repr__(self):
         return f"Book({self.isbn},{self.title},{self.author},{self.year})"
     
 class Library:
     def __init__(self):
-        self.books=[]
+        self.books=[] #empty list to store books
 
     def add_book(self,book):
-        self.books.append(book)
+        self.books.append(book) #append or add book to the list
 
     def borrow_book(self,isbn):
         for book in self.books:
-            if book.isbn == isbn and not book.is_borrowed:
-                book.is_borrowed=True
+            if book.isbn == isbn and not book.is_borrowed: #it comapres isbn wich is enterd with the isbn in library's book and checks that it is not borrowed
+                book.is_borrowed=True #if above condition is true it changes the value to true
                 return
-        raise ValueError("Book is not available or there is no book of this name")
+        raise ValueError("Book is not available or there is no book of this name")#if condition is false it returns error msg
     
     def return_book(self,isbn):
         for book in self.books:
-            if book.isbn == isbn and book.is_borrowed:
-                book.is_borrowed=False
+            if book.isbn == isbn and book.is_borrowed: #checks that isdn mathes or not and book is borrowed
+                book.is_borrowed=False #if book is borrowed than it changes the status to false(not borrowed)
                 return
         raise ValueError("this book was not borrowed or it does not exists")
     
     def view_available_books(self):
-        return [book for book in self.books if not book.is_borrowed]
+        return [book for book in self.books if not book.is_borrowed] #returns list of books in which is_borrowed is false
 
 #creating menu for all the functionalities so that user can easily access this
 def main():
-    library = Library()
+    library = Library() #initialize
 
     #display the menu
     while True:
@@ -46,19 +46,21 @@ def main():
         print("4. View Available Books")
         print("5. Exit")
 
-        ch=input("enter your choice")
+        ch=input("enter your choice")#take user input
 
+        #check which is the choice
         if ch=="1":
+            #enter details of book
             isbn=input("enter isbn: ")
             title=input("enter title for book: ")
             author=input("enter author of the book: ")
             year=input("enter publication year: ")
-            book=Book(isbn=isbn, title=title, author=author, year=year)
-            library.add_book(book)
+            book=Book(isbn=isbn, title=title, author=author, year=year)#compares entered details with Book class's details
+            library.add_book(book)#add book
             print(f"!!!!----book '{title}' added succesfully---!!!")
 
         elif ch=="2":
-            isbn=input("enter isbn of the book to borrow: ")
+            isbn=input("enter isbn of the book to borrow: ")#take isbn to borrow book
             try:
                 library.borrow_book(isbn)
                 print("book borrowed successfully!!!!")
@@ -74,8 +76,8 @@ def main():
                 print(e)
             
         elif ch=="4":
-            available_books=library.view_available_books()
-            if available_books:
+            available_books=library.view_available_books()#list of available books
+            if available_books:#if books are available display details
                 print("AVAILABLE BOOKS: ")
                 for book in available_books:
                     print(f"ISBN : {book.isbn},TITLE : {book.title} , AUTHOR : {book.author} , YEAR : {book.year}")
@@ -85,6 +87,9 @@ def main():
         elif ch=="5":
             print("Good Bye!!")
             break
+        
+        else:
+            print("invalid choice")
 
 if __name__ == "__main__":
     main()
--- test_library.py
+++ test_library.py
@@ -3,28 +3,28 @@ from library import Library,Book
 
 class TestLibraryManagementSystem(unittest.TestCase):
     def setUp(self):
-        self.library=Library()
+        self.library=Library() #create new instance before each test
 
     #test for add book
     def test_add_book(self):
          book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
          self.library.add_book(book)
-         self.assertIn(book,self.library.books)
+         self.assertIn(book,self.library.books) #to check that book is available in books list or not
 
     #test for borrow book
     def test_borrow_book(self):
          book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
-         self.library.add_book(book)
-         self.library.borrow_book("1234567890")
-         self.assertTrue(book.is_borrowed)
+         self.library.add_book(book) #to add book in library #borrow by passing isbn
+         self.library.borrow_book("1234567890") #borrow by passing isbn
+         self.assertTrue(book.is_borrowed) #it checks if is_borrowed flag is true?
 
     #test for return book
     def test_return_book(self):
         book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
         self.library.add_book(book)
         self.library.borrow_book("1234567890")
-        self.library.return_book("1234567890")
-        self.assertFalse(book.is_borrowed)
+        self.library.return_book("1234567890") #return book using isdn
+        self.assertFalse(book.is_borrowed)#it checks if is_borrowed false?
 
     #test for available books
     def test_available_book(self):
@@ -33,9 +33,9 @@ class TestLibraryManagementSystem(unittest.TestCase):
         self.library.add_book(book1)
         self.library.add_book(book2)
         self.library.borrow_book("1234567890")
-        available_books=self.library.view_available_books()
-        self.assertNotIn(book1,available_books)
-        self.assertIn(book2,available_books)
+        available_books=self.library.view_available_books()#store th list of available books
+        self.assertNotIn(book1,available_books)#to check that not available(borrowed) book is not in available_books
+        self.assertIn(book2,available_books)#to check that available(not borrowed) book is in available_books
         
 if __name__ == "__main__":
     unittest.main()
