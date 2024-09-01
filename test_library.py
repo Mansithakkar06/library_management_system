18095a71e27026584898f4636f2b6d3b1cc9bcfc
Mansithakkar06 <mansithakkar203@gmail.com>
2024-09-01 14:25:29 +0530

    test for returning books

--- test_library.py
+++ test_library.py
@@ -5,16 +5,26 @@ class TestLibraryManagementSystem(unittest.TestCase):
     def setUp(self):
         self.library=Library()
 
+    #test for add book
     def test_add_book(self):
          book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
          self.library.add_book(book)
          self.assertIn(book,self.library.books)
 
+    #test for borrow book
     def test_borrow_book(self):
          book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
          self.library.add_book(book)
          self.library.borrow_book("1234567890")
          self.assertTrue(book.is_borrowed)
+
+    #test for return book
+    def test_return_book(self):
+        book = Book(isbn="1234567890", title="Python Programming", author="John Doe", year=2020)
+        self.library.add_book(book)
+        self.library.borrow_book("1234567890")
+        self.library.return_book("1234567890")
+        self.assertFalse(book.is_borrowed)
         
 if __name__ == "__main__":
     unittest.main()
